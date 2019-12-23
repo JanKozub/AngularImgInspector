cd09c1a1f7ffc767f33eb6b8dccea00899f00e0e
Jan <j.kozub136@gmail.com>
2019-12-23 20:23:50 +0100

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ImgSliderComponent } from './img-slider/img-slider.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ImgSliderComponent
   ],
   imports: [
     BrowserModule
--- src/app/img-slider/img-slider.component.ts
+++ src/app/img-slider/img-slider.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-img-slider',
@@ -7,9 +8,129 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ImgSliderComponent implements OnInit {
 
-  constructor() { }
+  @Input()
+  wrapperW: number;
+  @Input()
+  wrapperH: number;
+  @Input()
+  imgW: number;
+  @Input()
+  imgH: number;
+  @Input()
+  overflowAnimationSpeed = 100;
 
-  ngOnInit() {
+  interval: number;
+  mouseX: number;
+  mouseY: number;
+  imgContainerX: number;
+  imgContainerY: number;
+
+  constructor() {
   }
 
+  ngOnInit() {
+    $('#cover').css({
+      width: this.imgW,
+      height: this.imgH,
+      top: 0
+    });
+
+    const imgContainer = $('#img-container');
+    imgContainer.css({
+      width: this.imgW,
+      height: this.imgH
+    });
+    $('#wrapper').css({
+      width: this.wrapperW,
+      height: this.wrapperH
+    });
+
+    window.addEventListener('mousemove', (e) => this.onMouseMoveUpdate(e), false);
+    window.addEventListener('mouseenter', (e) => this.onMouseMoveUpdate(e), false);
+    this.imgContainerX = this.getPropNum('#img-container', 'left');
+    this.imgContainerY = this.getPropNum('#img-container', 'top');
+
+    if (!(this.imgH > this.wrapperH)) {  // is Y axis available
+      this.imgContainerY = (this.wrapperH - this.imgH) / 2;
+      imgContainer.css('top', this.imgContainerY);
+    }
+
+    this.setPosition();
+    this.checkOverflow();
+  }
+
+  onMouseMoveUpdate(e) {
+    this.mouseX = e.pageX;
+    this.mouseY = e.pageY;
+  }
+
+  getPropNum(selector: string, prop: string) {
+    return +$(selector).css(prop).replace(/[^-\d.]/g, '');
+  }
+
+  checkOverflow() {
+    $(document).on('mouseleave mouseup', () => {
+
+      this.checkOverflowX();
+
+      if (this.imgH > this.wrapperH) {
+        this.checkOverflowY();
+      }
+
+      $('#img-container').animate({left: this.imgContainerX, top: this.imgContainerY}, this.overflowAnimationSpeed);
+
+      window.clearInterval(this.interval);
+    });
+  }
+
+  checkOverflowX() {
+    const halfBoxW = (this.wrapperW / 2);
+    if (this.imgContainerX > (this.wrapperW - halfBoxW)) {
+      this.imgContainerX = (this.wrapperW - halfBoxW);
+    }
+    if ((this.imgContainerX + this.imgW) < halfBoxW) {
+      this.imgContainerX = (this.imgW * -1) + halfBoxW;
+    }
+  }
+
+  checkOverflowY() {
+    const halfBoxH = (this.wrapperH / 2);
+    if (this.imgContainerY > this.wrapperH - halfBoxH) {
+      this.imgContainerY = (this.wrapperH - halfBoxH);
+    }
+    if ((this.imgContainerY + this.imgH) < halfBoxH) {
+      this.imgContainerY = (this.imgH * -1) + halfBoxH;
+    }
+  }
+
+  setPosition() {
+    $('#img-container').on('mousedown', () => {
+      const lastX = this.mouseX;
+      const lastY = this.mouseY;
+      const lastObjX = this.imgContainerX;
+      const lastObjY = this.imgContainerY;
+      let lastDistanceX = 0;
+      let lastDistanceY = 0;
+      this.interval = window.setInterval(() => {
+        const distanceToMoveX = this.mouseX - lastX;
+        if (distanceToMoveX !== lastDistanceX) {
+          lastDistanceX = distanceToMoveX;
+          this.imgContainerX = lastObjX + distanceToMoveX;
+        }
+        if ((this.imgH > this.wrapperH)) {
+          const distanceToMoveY = this.mouseY - lastY;
+          if (distanceToMoveY !== lastDistanceY) {
+            lastDistanceY = distanceToMoveY;
+            this.imgContainerY = lastObjY + distanceToMoveY;
+          }
+        } else {
+          this.imgContainerY = (this.wrapperH - this.imgH) / 2;
+        }
+        $('#img-container').css({
+          left: this.imgContainerX,
+          top: this.imgContainerY
+        });
+      }, 10);
+    });
+  }
 }
